6dd53ce32aa98fc809b7bd7024577b8c68840ccf
ShehryaAhmed <shehreyarahmed5@gmail.com>
2016-12-05 02:20:19 +0500

    now add progressbar,listview etc...

--- app/src/main/java/com/example/android/friendlychat/MainActivity.java
+++ app/src/main/java/com/example/android/friendlychat/MainActivity.java
@@ -2,9 +2,27 @@ package com.example.android.friendlychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
+    public static final String ANONYMOUS = "anonymous";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+
+    private ListView mMessageListView;
+    private MessageAdapter mMessageAdapter;
+    private ProgressBar mProgressBar;
+    private ImageButton mPhotoPickerButton;
+    private EditText mMessageEditText;
+    private Button mSendButton;
+
+    private String mUsername;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
