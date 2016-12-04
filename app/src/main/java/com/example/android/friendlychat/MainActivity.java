6d3896b8d705d811e42bcf63a80191f02dc90403
ShehryaAhmed <shehreyarahmed5@gmail.com>
2016-12-05 02:24:06 +0500

    now work on xml activity

--- app/src/main/java/com/example/android/friendlychat/MainActivity.java
+++ app/src/main/java/com/example/android/friendlychat/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mSendButton;
 
     private String mUsername;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
